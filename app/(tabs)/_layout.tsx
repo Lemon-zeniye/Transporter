9cee9b3191ac33e8bdffbbc74bb5ee0b27a0847a
mahlet <mahlet@gmail.com>
2024-10-29 15:05:30 +0300

    layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  FontAwesome,
+  FontAwesome5,
+  FontAwesome6,
+} from "@expo/vector-icons";
 import { View, Text, StyleSheet } from "react-native";
 import { usePathname } from "expo-router";
-import HomeScreen from "./home"; // Import your screens
+import Order from "./home"; // Import your screens
 import ProfileScreen from "./profile";
 import Handoff from "./handoff";
+import Active from "./status";
 
 // Define types for Tab Navigator
 type TabParamList = {
-  Home: undefined;
+  Order: undefined;
   Profile: undefined;
   Handoff: undefined;
   Active: undefined;
@@ -25,24 +32,25 @@ function TabNavigator() {
     <Tab.Navigator
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color, size }) => {
-          let iconName: keyof typeof Ionicons.glyphMap;
-          if (route.name === "Home") {
-            iconName = "home-outline";
+          if (route.name === "Order") {
+            return <FontAwesome6 name="box" color={color} size={size} />;
+          } else if (route.name === "Active") {
+            return <FontAwesome6 name="box-open" color={color} size={size} />;
           } else if (route.name === "Handoff") {
-            iconName = "swap-horizontal-outline"; // Choose an icon for Handoff
+            return <FontAwesome name="handshake-o" color={color} size={size} />;
           } else if (route.name === "Profile") {
-            iconName = "person-outline";
+            return <FontAwesome5 name="user" color={color} size={size} />;
           }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return null;
         },
-        tabBarActiveTintColor: "#5363df",
-        tabBarInactiveTintColor: "#000",
+        tabBarActiveTintColor: "#5363df", // Active color for icon and text
+        tabBarInactiveTintColor: "#000", // Inactive color for icon and text
         tabBarLabelStyle: styles.tabBarLabel,
         tabBarStyle: { backgroundColor: "#fff", paddingBottom: 5 },
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Active" component={Active} />
       <Tab.Screen name="Handoff" component={Handoff} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
